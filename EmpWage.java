91538c76946fbb5bf048a829374bebe4631d549d
mebangit123 <mebannongrum333@gmail.com>
2021-04-12 18:18:15 +0530

    Added logic for calculating employee daily wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,12 +1,26 @@
 public class EmpWage{
 	public static void main(String args[]){
 
-			int is_Present = 1;
-			double empCheck = Math.floor(Math.random() * 10) % 2;
+		int is_Present = 1;
+		int wage_per_hr=20;
+		int full_day_hr=8;
+		int emp_work_hrs=0;
+		int daily_wage=0;
 
-			if(empCheck == is_Present)
-				System.out.println("Employee is Present");
-			else
-				System.out.println("Employee is Absent");
+			while(emp_work_hrs < full_day_hr)
+				{
+					double empCheck = Math.floor(Math.random() * 10) % 2;
+
+					if(empCheck == is_Present)
+						{
+							daily_wage = emp_work_hrs * wage_per_hr;
+						}
+					else
+						{
+							daily_wage+=0;
+						}
+					emp_work_hrs++;
+				}
+		System.out.println(daily_wage);
 	}
 }

ae62fffaa4d37fea5434dcad4c2aaf967b5807f7
mebangit123 <mebannongrum333@gmail.com>
2021-04-12 16:49:27 +0530

    Check employee absent or present logic is added

--- EmpWage.java
+++ EmpWage.java
@@ -1,5 +1,12 @@
 public class EmpWage{
 	public static void main(String args[]){
-			System.out.println("Welcome to Employee Wage Computation program on Master Branch");
+
+			int is_Present = 1;
+			double empCheck = Math.floor(Math.random() * 10) % 2;
+
+			if(empCheck == is_Present)
+				System.out.println("Employee is Present");
+			else
+				System.out.println("Employee is Absent");
 	}
 }

62c63cc84fe5699605cf0bbe00cfde9a4012d6da
mebangit123 <mebannongrum333@gmail.com>
2021-04-12 18:57:07 +0530

    Added part time employee and wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,24 +1,30 @@
 public class EmpWage{
 	public static void main(String args[]){
 
-		int is_Present = 1;
-		int wage_per_hr=20;
-		int full_day_hr=8;
-		int emp_work_hrs=0;
-		int daily_wage=0;
+		int is_FullTime = 1;
+		int is_PartTime = 2;
+		int wage_per_hr = 20;
+		int part_time_hr = 8;
+		int full_day_hr = 8;
+		int emp_work_hrs = 0;
+		int daily_wage = 0;
 
 			while(emp_work_hrs < full_day_hr)
 				{
-					double empCheck = Math.floor(Math.random() * 10) % 2;
+					double empCheck = Math.floor(Math.random() * 10) % 3;
 
-					if(empCheck == is_Present)
-						{
-							daily_wage = emp_work_hrs * wage_per_hr;
-						}
+					if(empCheck == is_FullTime)
+					{
+						daily_wage = emp_work_hrs * wage_per_hr;
+					}
+					else if(empCheck == is_PartTime)
+					{
+						daily_wage = emp_work_hrs * part_time_hr;
+					}
 					else
-						{
-							daily_wage+=0;
-						}
+					{
+						daily_wage+=0;
+					}
 					emp_work_hrs++;
 				}
 		System.out.println(daily_wage);

ccb8c9a0845389743e65ad1ac83edffdb22b4fff
mebangit123 <mebannongrum333@gmail.com>
2021-04-13 20:25:43 +0530

    added calculating wage for a month

--- EmpWage.java
+++ EmpWage.java
@@ -1,25 +1,31 @@
 public class EmpWage{
 	public static void main(String args[]){
 
-		int is_Present = 1;
-		int wage_per_hr=20;
-		int full_day_hr=8;
-		int emp_work_hrs=0;
+		int is_FullTime = 1;
+		int is_PartTime = 2;
+		int wage_per_day=20;
+		int part_time_wage = 8;
+		int max_day_in_month = 20;
+		int emp_work_day=0;
 		int daily_wage=0;
 
-			while(emp_work_hrs < full_day_hr)
+			while(emp_work_day < max_day_in_month)
 				{
 					double empCheck = Math.floor(Math.random() * 10) % 2;
 
-					if(empCheck == is_Present)
+					if(empCheck == is_FullTime)
 						{
-							daily_wage = emp_work_hrs * wage_per_hr;
+							daily_wage = emp_work_day * wage_per_day;
+						}
+					else if(empCheck == is_PartTime)
+						{
+							daily_wage = emp_work_day * part_time_wage;
 						}
 					else
 						{
 							daily_wage+=0;
 						}
-					emp_work_hrs++;
+					emp_work_day++;
 				}
 		System.out.println(daily_wage);
 	}

341ac1697243880386f167d31d1c2e657304472e
mebangit123 <mebannongrum333@gmail.com>
2021-04-13 19:38:05 +0530

    Solving using switch case statement

--- EmpWage.java
+++ EmpWage.java
@@ -1,6 +1,7 @@
-public class EmpWage{
-	public static void main(String args[]){
-
+public class EmpWage
+{
+	public static void main(String args[])
+	{
 		int is_FullTime = 1;
 		int is_PartTime = 2;
 		int wage_per_hr = 20;
@@ -13,17 +14,16 @@ public class EmpWage{
 				{
 					double empCheck = Math.floor(Math.random() * 10) % 3;
 
-					if(empCheck == is_FullTime)
+					switch(empCheck)
 					{
-						daily_wage = emp_work_hrs * wage_per_hr;
-					}
-					else if(empCheck == is_PartTime)
-					{
-						daily_wage = emp_work_hrs * part_time_hr;
-					}
-					else
-					{
-						daily_wage+=0;
+						case 1:
+							daily_wage = emp_work_hrs * wage_per_hr;
+							break;
+						case 2:
+							daily_wage = emp_work_hrs * part_time_hr;
+							break;
+						default:
+							daily_wage += 0;
 					}
 					emp_work_hrs++;
 				}

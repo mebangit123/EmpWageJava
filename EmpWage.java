f4a86c1376e927be4c8f08abdbc3da9d284fbe2d
mebangit123 <mebannongrum333@gmail.com>
2021-04-14 08:20:54 +0530

    Added condition for calculating wage for a month

--- EmpWage.java
+++ EmpWage.java
@@ -1,32 +1,53 @@
-public class EmpWage{
-	public static void main(String args[]){
+class EmpWageCheck {
 
 		int is_FullTime = 1;
 		int is_PartTime = 2;
-		int wage_per_day=20;
-		int part_time_wage = 8;
-		int max_day_in_month = 20;
-		int emp_work_day=0;
-		int daily_wage=0;
 
-			while(emp_work_day < max_day_in_month)
+		public int getWorkingHours(double n){
+
+			int emp_Hours=0;
+
+				if(n == is_FullTime)
 				{
-					double empCheck = Math.floor(Math.random() * 10) % 3;
-
-					if(empCheck == is_FullTime)
-						{
-							daily_wage = emp_work_day * wage_per_day;
-						}
-					else if(empCheck == is_PartTime)
-						{
-							daily_wage = emp_work_day * part_time_wage;
-						}
-					else
-						{
-							daily_wage+=0;
-						}
-					emp_work_day++;
+					emp_Hours=3;
 				}
-		System.out.println(daily_wage);
+				else if(n == is_PartTime)
+				{
+					emp_Hours=2;
+				}
+				else
+				{
+					emp_Hours=0;
+				}
+				return emp_Hours;
+			}
+}
+
+public class EmpWage{
+	public static void main(String args[]){
+
+		//constant
+		int max_hrs_in_month = 100;
+		int emp_rate_per_hr = 15;
+		int num_working_days = 20;
+
+		int total_emp_hr = 0;
+		int totalWorkingDays = 0;
+		float salary = 0;
+
+		EmpWageCheck emp = new EmpWageCheck();
+
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+
+
+		while(total_emp_hr < max_hrs_in_month & totalWorkingDays < num_working_days)
+			{
+				int workHours = emp.getWorkingHours(empCheck);
+				total_emp_hr += workHours;
+				salary = total_emp_hr * emp_rate_per_hr;
+				totalWorkingDays++;
+			}
+		System.out.println(salary);
 	}
 }
+
